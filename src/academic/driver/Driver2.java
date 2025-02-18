c291b9378eb7d440a95c796d01070c19c39e9e90
PoppySibuea <143679506+PoppySibuea@users.noreply.github.com>
2025-02-18 13:40:38 +0700

    pair 8

--- src/academic/driver/Driver2.java
+++ src/academic/driver/Driver2.java
@@ -1,4 +1,10 @@
 package academic.driver;
+import java.util.ArrayList;
+import java.util.Scanner;
+import academic.model.Student;
+import academic.model.Course;
+import academic.model.Enrollment;
+
 
 /**
  * @author 12S23026 - Arif Doloksaribu
@@ -9,7 +15,42 @@ public class Driver2 {
     public static void main(String[] _args) {
 
         // codes
+        Scanner sc = new Scanner (System.in);
+        ArrayList<Course> courses = new ArrayList<Course>();
+        ArrayList<Student> stu = new ArrayList<Student>();
+        ArrayList<Enrollment> enrol = new ArrayList<Enrollment>();
 
+        while (true){
+            String input = sc.nextLine();
+            if(input.equals("---")){
+                break;
+            }
+
+            String[] data = input.split("#");
+
+            if (data[0].equals("course-add")){
+            courses.add(new Course(data[1], data[2], Integer.parseInt(data[3]), data[4]));
+            }
+            else if (data[0].equals("student-add")){
+            stu.add(new Student(data[1], data[2], data[3], data[4]));
+            }
+            else if (data[0].equals("enrollment-add")){
+            enrol.add(new Enrollment(data[1], data[2], data[3], data[4]));
+            }
+        }
+    
+        for (int i = courses.size() - 1; i >= 0; i--) {
+            System.out.println(courses.get(i).toString());
+        }
+
+        for (int i = 0; i < stu.size(); i++) {
+            System.out.println(stu.get(i).toString());
+        }
+
+        for (int i = 0; i < enrol.size(); i++) {
+            System.out.println(enrol.get(i).toString());
+        }
+        sc.close();
     }
 
 }
